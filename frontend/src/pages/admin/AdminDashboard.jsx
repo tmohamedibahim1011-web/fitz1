92c4aec78d9c96e02919accb7891b1c790dc06d1
MohamedCoderX <mdibrahim.developer@gmail.com>
2026-06-02 11:33:13 +0530

    Asdasdas:

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -801,7 +801,7 @@ const AdminDashboard = () => {
                                         : order.customerInfo.phone.replace(/\D/g, '')) 
                                     : ''
                                 }?text=${encodeURIComponent(
-                                  `Hey ${order.customerInfo?.firstName || ''}, you are one step ahead of buying the fitz1 product! May I know what is the reason behind the payment not being done?`
+                                  `Hi ${order.customerInfo?.firstName ? `${order.customerInfo.firstName} ${order.customerInfo.lastName || ''}`.trim() : 'Customer'},\n\nI am Mr.Dhayanath from Fitz1 Official Sales and Customer support!\nYou are one step away from purchasing our Parallettes.\n\nDo you need any assistance in completing the order sir? Please Ignore if already paid .`
                                 )}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
